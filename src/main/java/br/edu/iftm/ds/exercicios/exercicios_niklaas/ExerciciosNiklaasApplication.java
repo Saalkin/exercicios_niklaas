e736755fc320a3d9289f2920885c14835705123c
Niklaas <niklaaspedro@gmail.com>
2024-08-06 22:51:57 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/ds/exercicios/exercicios_niklaas/ExerciciosNiklaasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exercicios.exercicios_niklaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosNiklaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosNiklaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/ds/exercicios/exercicios_niklaas/ExerciciosNiklaasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exercicios.exercicios_niklaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosNiklaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
